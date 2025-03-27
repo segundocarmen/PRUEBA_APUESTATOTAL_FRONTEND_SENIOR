31857b8eb69768b6958b6a7674ba4d16758b9323
segundo carmen dominguez <scarmen@bzero.cl>
2025-03-27 11:48:38 -0500

    update

--- shell/dist/assets/__federation_shared_react-dom-C8HiEUq4.js
+++
@@ -1,7 +0,0 @@
-import { g as getDefaultExportFromCjs } from './_commonjsHelpers-B85MJLTf.js';
-import { r as requireReactDom } from './index-brvGd106.js';
-
-var reactDomExports = requireReactDom();
-const index = /*@__PURE__*/getDefaultExportFromCjs(reactDomExports);
-
-export { index as default };
--- shell/src/components/logout/index.tsx
+++ shell/src/components/logout/index.tsx
@@ -1,12 +1,15 @@
 import { useNavigate } from 'react-router';
 import { DeleteStorage } from '../../common/Storage';
 import './style.scss';
+import { usePokemon } from '../../hooks/usePokemon';
 
 const Logout = () => {
     const navigate = useNavigate();
+    const { setPokemonSearchReplace } = usePokemon();
     const OnLogout = () => {
         DeleteStorage(import.meta.env.VITE_USER_CONECTED_KEY);
         DeleteStorage(import.meta.env.VITE_POKEMON_HISTORY_KEY);
+        setPokemonSearchReplace([]);
         navigate('/');
     };
 
